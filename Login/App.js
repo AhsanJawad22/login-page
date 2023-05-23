abefa43fb0427415542c7c2e1e3a7fe876ba07c6
AhsanJawad22 <121507473+AhsanJawad22@users.noreply.github.com>
2023-05-23 12:54:16 -0700

    Add files via upload
    flie edit

---
+++ Login/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import Login from "./components/Login";
+import Home from "./components/Home";
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return ( 
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="LoginPage">
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
---
+++ Login/components/Home.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
+import * as DocumentPicker from "expo-document-picker";
+
+const Home = () => {
+  const handleFilePick = async () => {
+    try {
+      const file = await DocumentPicker.getDocumentAsync();
+
+      if (file.type === "success") {
+        console.log(file);
+      }
+    } catch (error) {   
+      console.log(error);
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Button onPress={handleFilePick} title="Pick a file" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    paddingHorizontal: 16,
+  },
+  // body: {
+  //   backgroundColor:"green"
+  // }
+});
+
+export default Home;
